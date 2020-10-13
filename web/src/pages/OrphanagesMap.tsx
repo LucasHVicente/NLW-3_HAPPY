67ceeed93f5252e937c3ae5c61b15deef86cfe81
Vince <l.vicente9003@gmail.com>
2020-10-13 17:06:36 -0300

    maps

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -29,7 +29,9 @@ function OrphanagesMap(){
           height: '100%'
         }}
       >
-        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {/* Escolher uma das opções de mapa para ser utilizado, ambas funcionam */}
+        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+        <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}/>
       </Map>
       
       
