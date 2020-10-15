a9310687f70194d2fe98a21ae111b27432ad842e
Vince <l.vicente9003@gmail.com>
2020-10-14 22:29:13 -0300

    implementado cors, tratamento de exceções e views

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,6 +1,9 @@
 import {getRepository} from 'typeorm';
 import Orphanage from '../models/Orphanage';
 import{Request, Response} from 'express';
+import orphanageView from '../views/orphanages_view';
+import * as Yup from 'yup';
+
 
 export default{
 
@@ -9,9 +12,11 @@ export default{
 
     const orphanagesRepository = getRepository(Orphanage);
 
-    const orphanage = await orphanagesRepository.findOneOrFail(id);
+    const orphanage = await orphanagesRepository.findOneOrFail(id, {
+      relations: ['images']
+    });
 
-    return response.json(orphanage)
+    return response.json(orphanageView.render(orphanage))
 
   },
 
@@ -22,7 +27,7 @@ export default{
       relations: ['images']
     });
 
-    return response.json(orphanages)
+    return response.json(orphanageView.renderMany(orphanages))
 
   },
 
@@ -46,7 +51,7 @@ export default{
       return {path: image.filename}
     })
 
-    const orphanage = orphanagesRepository.create({
+    const data = {
       name, 
       latitude, 
       longitude, 
@@ -55,7 +60,28 @@ export default{
       opening_hours, 
       open_on_weekends,
       images
+    }
+
+    const shcema = Yup.object().shape({
+      name: Yup.string().required(), 
+      latitude: Yup.number().required(), 
+      longitude: Yup.number().required(), 
+      about: Yup.string().required().max(300), 
+      instructions: Yup.string().required(), 
+      opening_hours: Yup.string().required(), 
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(
+        Yup.object().shape({
+          path: Yup.string().required()
+        })
+      )
+    })
+
+    await shcema.validate(data, {
+      abortEarly: false,
     });
+    
+    const orphanage = orphanagesRepository.create(data);
   
     await orphanagesRepository.save(orphanage);
   
